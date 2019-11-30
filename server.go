652a09ebd8f465d350ee3aecf78c27eb07e55f95
yuichi kishimoto <yk2220s@gmail.com>
2019-11-30 23:21:19 +0900

    use di to build modules

--- server.go
+++ server.go
@@ -1,47 +1,19 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"net"
 
-	pb "github.com/yk2220s/go-grpc-sample/blog"
-	"google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	"google.golang.org/grpc/reflection"
-	status "google.golang.org/grpc/status"
+	"github.com/yk2220s/go-grpc-sample/api/application/di"
 )
-
-const (
-	port = 50001
-)
-
-type blogServer struct {
-	pb.UnimplementedBlogServer
-}
-
-func (s *blogServer) GetPost(ctx context.Context, req *pb.GetRequest) (*pb.Post, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetPost not implemented")
-}
-
-func (s *blogServer) SavePost(ctx context.Context, req *pb.SaveRequest) (*pb.Post, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SavePost not implemented")
-}
 
 func main() {
-	fmt.Println("building server..")
+	server, cleanup, err := di.InitializeGRPCServer()
 
-	address := fmt.Sprintf("localhost:%d", port)
-	lis, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to build server: %v", err)
 	}
 
-	s := grpc.NewServer()
-	pb.RegisterBlogServer(s, &blogServer{})
-	reflection.Register(s)
+	defer cleanup()
 
-	fmt.Println("server running on " + address)
-	s.Serve(lis)
+	server.Serve()
 }
